a18c9517da0d37b368542b4d91b10dc5f2ede4ed
Nandan Wewhare <nandanwewhare25@gmail.com>
2020-12-30 10:45:28 +0530

    minor refactoring

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ import "./index.css";
 
 const books = [
   {
+    id: 1, // to remove => Warning: Each child in a list should have a unique "key" prop.
     img:
       "https://images-eu.ssl-images-amazon.com/images/I/61MrRA4qE0L._AC_UL200_SR200,200_.jpg",
     title: "INDIAN POLITY",
     author: "M LAXMIKANTH",
   },
   {
+    id: 2,
     img:
       "https://images-eu.ssl-images-amazon.com/images/I/61r-WomIz0L._AC_UL200_SR200,200_.jpg",
     title: "One Arranged Murder",
@@ -21,7 +23,9 @@ function BookList() {
   return (
     <section className="bookList">
       {books.map((book) => {
-        return <Book book={book}></Book>;
+        return <Book book={book}></Book>; 
+        // instead of passing object we can also pass {...book} which passes all the properties to the function
+        // overall it will become <Book {...book}/>
       })}
     </section>
   );
